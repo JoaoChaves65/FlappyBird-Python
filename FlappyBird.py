efb9ef83d05daded03e810c17972317137a017df
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-15 21:54:30 -0300

    Criando as constantes do jogo

---
+++ FlappyBird.py
@@ -0,0 +1,24 @@
+import pygame
+import os
+import random
+
+TELA_ALTURA = 500
+TELA_LARGURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("imgs", "pipe.png"))
+)
+IMAGEM_CHAO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("imgs", "base.png"))
+)
+IMAGEM_BACKGROUND = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("imgs", "bg.png"))
+)
+IMAGES_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png"))),
+]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont("arial", 50)
